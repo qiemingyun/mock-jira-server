2d907879fa904681f9d336c31aac2502c69a1e79
mingyun <qiemingyuny@gmail.com>
2021-11-29 17:46:38 +1100

    mysql

--- conf/db.js
+++ conf/db.js
@@ -5,9 +5,9 @@ let REDIS_CONF
 
 if (env === 'dev') {
   MYSQL_CONF = {
-    host: '3.129.226.65',
+    host: '3.134.146.251',
     user: 'qmy',
-    password: 'Tr.20201126',
+    password: 'Tr20201126',
     port: '3306',
     database: 'jira',
     multipleStatements: true
@@ -21,9 +21,9 @@ if (env === 'dev') {
 
 if (env === 'prd') {
   MYSQL_CONF = {
-    host: '3.129.226.65',
+    host: '3.134.146.251',
     user: 'qmy',
-    password: 'Tr.20201126',
+    password: 'Tr20201126',
     port: '3306',
     database: 'jira',
     multipleStatements: true
